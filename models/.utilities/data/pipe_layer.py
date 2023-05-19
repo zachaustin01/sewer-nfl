c3a3a09010ca7641a7b8870821a28f0455db50d0
Zach Austin <76120216+zachaustin01@users.noreply.github.com>
2023-05-19 00:05:22 -0500

    Pylint

--- models/.utilities/data/pipe_layer.py
+++ models/.utilities/data/pipe_layer.py
@@ -1,16 +1,16 @@
-# Goal: Identify pipelines from input file (source.yml) and run them on initialization to build modeling dataset.and
 
-REPO_NAME = 'sewer-nfl'
-import sys, os
-cwd = str(os.getcwd())
-repo_dir = cwd[:cwd.find(REPO_NAME)+len(REPO_NAME)]
-sys.path.insert(0,repo_dir)
-
-
+import sys
+import os
 import pandas as pd
 import yaml
 import importlib
 
+REPO_NAME = 'sewer-nfl'
+CWD = str(os.getcwd())
+REPO_DIR = CWD[:CWD.find(REPO_NAME)+len(REPO_NAME)]
+sys.path.insert(0,REPO_DIR)
+
+"""Module to build dataset from yml file."""
 def build_dataset(input_file = 'source.yml'):
     source_config = yaml.safe_load(open(input_file))
     feature = source_config['data'][0]
@@ -18,6 +18,5 @@ def build_dataset(input_file = 'source.yml'):
     mod = importlib.import_module(feature['file_path'], package=feature['function'])
     res = getattr(mod,feature['function'])(feature['args'])
     print(res)
-    
 
 build_dataset(input_file = './source_example.yml')
