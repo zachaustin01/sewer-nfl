d83f7dbd8c861b51903f63ff5df85b9165242170
Zach Austin <76120216+zachaustin01@users.noreply.github.com>
2023-05-19 15:58:42 -0500

    Update pipe_layer.py

--- models/.utilities/data/pipe_layer.py
+++ models/.utilities/data/pipe_layer.py
@@ -1,18 +1,18 @@
-
+"""Module to build dataset from yml file."""
 import sys
 import os
-import pandas as pd
-import yaml
 import importlib
+import yaml
 
 REPO_NAME = 'sewer-nfl'
 CWD = str(os.getcwd())
 REPO_DIR = CWD[:CWD.find(REPO_NAME)+len(REPO_NAME)]
 sys.path.insert(0,REPO_DIR)
 
-"""Module to build dataset from yml file."""
 def build_dataset(input_file = 'source.yml'):
-    source_config = yaml.safe_load(open(input_file))
+    """Function Docstring here"""
+    with open(input_file,encoding='utf-8') as file:
+        source_config = yaml.safe_load(file)
     feature = source_config['data'][0]
     print(feature)
     mod = importlib.import_module(feature['file_path'], package=feature['function'])
